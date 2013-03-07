6afd6fdd40e1bcdaf4248e87ea389c051f9894ae
Hugo Biarge <hbiarge@hotmail.com>
2013-03-07 18:48:04 +0100

    Añadidos test de IU con SpecFlow y WatiN

---
+++ UI.Test/Features/SituacionActual.feature
@@ -0,0 +1,11 @@
+﻿Feature: SituacionActual
+	In order to track my day
+	As a worker
+	I want to be able to start and stop my day
+
+Scenario: Begin day
+	Given I'm an authenticated as prueba with password prueba
+	And there is not a day started
+	When I press the start day button
+	Then the stop day button is showe
+	And the start pause button is showed
---
+++ UI.Test/Steps/BeginDaySteps.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BeginDaySteps.cs" company="Acheve Solutions">
+//   Copyright (c) Hugo Biarge. Todos los derechos reservados.
+// </copyright>
+// <summary>
+//   Defines the BeginDaySteps type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UI.Test.Steps
+{
+    using NUnit.Framework;
+
+    using TechTalk.SpecFlow;
+
+    using WatiN.Core;
+
+    [Binding]
+    public class BeginDaySteps
+    {
+        [Binding]
+        public class StepDefinitions
+        {
+            private readonly IE browser;
+
+            public StepDefinitions()
+            {
+                this.browser = new IE("http://localhost:51768/");
+            }
+
+            [Given(@"I'm an authenticated as (.*) with password (.*)")]
+            public void GivenIMAnAuthenticatedAsPruebaWithPasswordPrueba(string username, string password)
+            {
+                this.browser.TextField(Find.ById("UserName")).TypeText(username);
+                this.browser.TextField(Find.ById("Password")).TypeText(password);
+                this.browser.Button(Find.ById("btnEntrar")).Click();
+            }
+
+            [Given(@"there is not a day started")]
+            public void GivenThereIsNotADayStarted()
+            {
+            }
+
+            [When(@"I press the start day button")]
+            public void WhenIPressTheStartDayButton()
+            {
+                this.browser.Button(Find.ById("btnIniciarJornada")).Click();
+            }
+
+            [Then(@"the stop day button is showe")]
+            public void ThenTheStopDayButtonIsShowe()
+            {
+                Assert.IsNotNull(this.browser.Button(Find.ById("btnTerminarJornada")));
+            }
+
+            [Then(@"the start pause button is showed")]
+            public void ThenTheStartPauseButtonIsShowed()
+            {
+                Assert.IsNotNull(this.browser.Button(Find.ById("btnIniciarPausa")));
+            }
+        }
+    }
+}
